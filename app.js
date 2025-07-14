4c8127a924d3bc17311de7e0b8e50ee72941a6df
BeamDubai <142025143+BeamDubai@users.noreply.github.com>
2025-07-14 12:45:56 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const productRoutes = require('./routes/productRoutes');
+const authRoutes = require('./routes/authRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/cart', cartRoutes);
+
+const PORT =  3333;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
