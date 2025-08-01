2a5c1aef1d93677dd1167b561725dbaa98dd4de3
Your BeamDubai <beem094409@gmail.com>
2025-08-01 09:28:48 +0700

    update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/cart', cartRoutes);
 
-const PORT =  3333;
+const PORT =  process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
