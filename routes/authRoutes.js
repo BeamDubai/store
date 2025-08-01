21fa9bf34832002824710c08daff3993d29182fb
Your BeamDubai <beem094409@gmail.com>
2025-08-01 09:24:43 +0700

    update

---
+++ routes/authRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+// Add your auth route handlers here
+
+module.exports = router;
---
+++ routes/cartRoutes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+// Add your cart route handlers here
+
+module.exports = router;
